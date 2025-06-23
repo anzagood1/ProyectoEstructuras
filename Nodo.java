056181810a7bc8efae3f670757b1efc59581e3b7
anzagood1 <anzagood@espol.edu.ec>
2025-06-23 16:51:45 -0500

    Lista enlazada circular con su iterador integrado

---
+++ Nodo.java
@@ -0,0 +1,17 @@
+
+
+public class Nodo <T> {
+    public T dato; 
+    public Nodo<T>  next;
+    public Nodo<T> previous; 
+
+    public Nodo(T dato){
+        this.dato = dato;
+        this.next = null;
+        this.previous = null;
+    }
+    @Override
+    public String toString(){
+        return dato.toString();
+    }
+}
